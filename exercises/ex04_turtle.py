98edeea7f74b9c379c2533a2c8d582e1a31bf614
Ria Khetan <rkhetan@unc.edu>
2022-02-16 17:39:35 -0500

    finished!

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -1,15 +1,13 @@
-"""The program will draw a mountain and river landscape with fish in the river."""
+"""The program will draw a mountain, sun, and river landscape. For the fun part, I created a sun using the circle imported function on line 86. I wanted to see if I could make a circle without using a while loop. I have a total of four functions with one helper function. Complex functions are getting broken up in both the curve_river functions on line 26 and 49."""
 
 
 __author__ = "730476155"
-
-from turtle import Turtle, colormode, done, penup
-
+from turtle import Turtle, colormode, done
 colormode(255)
 
 
 def mountain_triangles(emily: Turtle, x: float, y: float, width: float) -> None:
-    """Draw five triangles of the given dimensions located at the top of the screen in a horizontal manner located at x,y."""
+    """Draw four triangles of the given dimensions located at the top of the screen in a horizontal manner located at x,y."""
     emily.goto(x, y)
     emily.setheading(0.0)
     emily.pendown()
@@ -23,11 +21,12 @@ def mountain_triangles(emily: Turtle, x: float, y: float, width: float) -> None:
     emily.penup()
 
 
-def zig_zag_river_down(emily: Turtle, x: float, y: float) -> None:
-    """Draws a zigzag line."""
+def curve_river_down(emily: Turtle, x: float, y: float) -> None:
+    """Draws a curvy line from top to bottom."""
+    make_a_straight_line(emily, 235, 140, -385)
     emily.setheading(180.0)  # which direction is the turtle pointing
-    emily.goto(x, y)
     emily.pendown()
+    
     i: int = 0
     counter: int = 0
     while (i < 6):
@@ -46,11 +45,12 @@ def zig_zag_river_down(emily: Turtle, x: float, y: float) -> None:
     emily.penup()
 
 
-def zig_zag_river_up(emily: Turtle, x: float, y: float) -> None:
-    """Draws a zigzag line."""
+def curve_river_up(emily: Turtle, x: float, y: float) -> None:
+    """Draw a curvy line from bottom to top."""
     emily.setheading(0.0)
     emily.goto(x, y)
     emily.pendown()
+    make_a_straight_line(emily, -496, -459, 455)
     i: int = 0
     counter: int = 0
     while (i < 6):
@@ -68,15 +68,17 @@ def zig_zag_river_up(emily: Turtle, x: float, y: float) -> None:
     emily.penup()
 
 
-def make_a_straight_line(emily: Turtle, x: float, y: float, width: float):
+def make_a_straight_line(emily: Turtle, x: float, y: float, width: float) -> None:
+    """Makes two straight lines connecting the two curvy lines."""
     emily.setheading(0.0)
     emily.goto(x, y)
     emily.pendown()
     emily.forward(width)
-    emily.penup()
+    # emily.penup()
 
 
-def sun_created_from_circle(emily: Turtle, x: float, y: float, radius):
+def sun_created_from_circle(emily: Turtle, x: float, y: float, radius: float) -> None:
+    """Makes a sun by calling the circle function."""
     emily.setheading(0.0)
     emily.goto(x, y)
     emily.pendown()
@@ -87,6 +89,7 @@ def sun_created_from_circle(emily: Turtle, x: float, y: float, radius):
 
 
 def main() -> None:
+    """Main Function, this is where all the functions get executed."""
     emily: Turtle = Turtle()
     emily.speed(5)
     emily.penup()
@@ -104,16 +107,8 @@ def main() -> None:
     emily.begin_fill()
     emily.color(32, 177, 217)
     emily.pensize(5)
-    # Right to Left
-    make_a_straight_line(emily, 235, 140, -385)
-    # Top to bottom (Left)
-    zig_zag_river_down(emily, -150, 140)
-    # Left to Right
-    make_a_straight_line(emily, -496, -459, 455)
-    print(emily.xcor())
-    print(emily.ycor())
-    # Bottom to Top (Right)
-    zig_zag_river_up(emily, -41, -459)
+    curve_river_down(emily, -150, 140)
+    curve_river_up(emily, -41, -459)
     emily.end_fill()
 
     emily.hideturtle()
