c50a1b8be2e7b12f2b04add581ff80cffb955383
Ria Khetan <rkhetan@unc.edu>
2022-04-02 09:35:20 -0400

    save!

--- lessons/vectorized.py
+++ lessons/vectorized.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import ItemsView
+from typing import Union
 
 
 class StrArray:
@@ -10,22 +10,29 @@ class StrArray:
 
     def __repr__(self) -> str:
         return f"StrArray({self.items})"
-
-    def __add__(self, rhs: str) -> StrArray:
+    
+    def __add__(self, rhs: Union[str, StrArray]) -> StrArray:
         """"Add is a vectorized operation that applies to all items.
         When rhs is a str, it is concatenated to every item in a new StrArray
         """
-        
-        # Establish a result list of strings that is empty
-        result: list[str] = list()
-        # Loop through every item in self.items
-        for item in self.items:
-            # Append the concatenation of item with rhs to your result list
-            result.append(item + rhs)
-        # After loop, return a new StrArray object constructed with result list
-        return StrArray(result)
+        if isinstance(rhs, str):
+            # Establish a result list of strings that is empty
+            result: list[str] = list()
+            # Loop through every item in self.items
+            for item in self.items:
+                # Append the concatenation of item with rhs to your result list
+                result.append(item + rhs)
+            # After loop, return a new StrArray object constructed with result list
+            return StrArray(result)
+        else:
+            result: list[str] = []
+            for i in range(0, len(self.items)):
+                result.append(self.items[i] + rhs.items[i])
+            return StrArray(result)
 
 
 first: StrArray = StrArray(["Armando", " Brady", " Caleb"])
 last: StrArray = StrArray(["Bacot", "Mankek", "Love"])
 print(first + "!")
+print(first)
+print(first + last)
