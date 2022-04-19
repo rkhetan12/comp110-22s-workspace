27fe307e9ae562fc7fbccfc961ca3b101e8d0407
Ria Khetan <rkhetan@unc.edu>
2022-04-19 16:46:27 -0400

    save

---
+++ exercises/ex10/linked_list_test.py
@@ -0,0 +1,18 @@
+"""Tests for linked list utils."""
+
+from multiprocessing.sharedctypes import Value
+from exercises.ex10.linked_list import Node, last
+
+__author__ = 730476155
+
+
+def test_last_empty() -> None:
+    """Last of an empty Linked List should raise an value error."""
+    with pytest.raises(ValueError):
+        last(None)
+
+
+def test_last_non_empty() -> None:
+    """Last of a non-empty list should return a reference to its last Node."""
+    linked_list = Node(1, Node(2, Node(3, None)))
+    assert last(linked_list) == 3
